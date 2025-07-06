313072847b72ce5b6ec5a6b115c772925395e003
TanishqRaj07 <tanishqraj.tp@gmail.com>
2025-07-06 23:55:35 +0530

    second commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/info")
+def lwinfo():
+        return "I am Lw from India"
+
+@app.route("/phone")
+def lwphone():
+        return "8003622849"
+
+app.run(host="0.0.0.0")
